5d24b6330d289417ee777a5e547492e12ac89fab
Pantelis Antonoudiou <pantelis71@hotmail.com>
2021-11-08 17:14:41 -0500

    update to pypi v0.0.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="adi-reader",
-    version="0.0.6",
+    version="0.0.8",
     author="Jim Hokanson",
     author_email="jim.hokanson@gmail.com",
     description="Reading LabChart recorded data",
