e15eb62862d3a8054eb3438c2a8f8d6480bf93b4
Rob Warnaar <89922216+rspwarnaarUT@users.noreply.github.com>
2025-09-26 16:58:37 +0200

    Update Python version to allow all version 3.13.*

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
     ],
-    python_requires='>= 3.6, <= 3.13',
+    python_requires='>= 3.6, < 3.14',
     include_package_data=True,
 )
